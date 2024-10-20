14eb9089e4a0af01411bca266286c8cdd9032332
EDavtyan <davtyan.edd@gmail.com>
2024-10-20 11:03:46 +0000

    added delta elevators experimentation for RFRegression

--- examples/RFRegression/delta_elevators.py
+++ examples/RFRegression/delta_elevators.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import xgboost as xgb
 import os
 import sys
 import json
@@ -11,6 +10,9 @@ from EIRegressor.model_optimizer import ModelOptimizer
 from EIRegressor.EmbeddedInterpreter import EmbeddedInterpreter
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+
+
 
 def execute(save_dir, n_buckets=3, i=None, bucketing_method="quantile", single_rules_breaks=3):
     # Load dataframe
@@ -31,29 +33,25 @@ def execute(save_dir, n_buckets=3, i=None, bucketing_method="quantile", single_r
         X, y, test_size=0.33)
 
     regressor_hp_grid = {
-        'regressor__learning_rate': [0.01, 0.05, 0.1],
         'regressor__n_estimators': [50, 100, 150],
-        'regressor__max_depth': [3, 5, 7],
-        'regressor__colsample_bytree': [0.7, 0.8, 0.9],
-        'regressor__gamma': [0, 0.1, 0.2],
-        'regressor__reg_alpha': [0, 0.1, 0.5],
-        'regressor__reg_lambda': [0.5, 1, 1.5]
+        'regressor__max_depth': [10, 20, 30],
+        'regressor__min_samples_split': [2, 5, 10],
+        'regressor__min_samples_leaf': [1, 2, 4],
+        'regressor__bootstrap': [True, False]
     }
 
     regressor_default_args = {
-        "learning_rate": 0.05,
         "n_estimators": 100,
-        "max_depth": 5,
-        "colsample_bytree": 0.8,
-        "gamma": 0.1,
-        "reg_alpha": 0.1,
-        "reg_lambda": 1
+        "max_depth": 20,
+        "min_samples_split": 2,
+        "min_samples_leaf": 1,
+        "bootstrap": True
     }
 
-    regressor_optimizer = ModelOptimizer(search_method="random", n_iter=450)
+    regressor_optimizer = ModelOptimizer(search_method="grid")
 
-    # Creation of EI Regression with XGBoost
-    eiReg = EmbeddedInterpreter(regressor=xgb.XGBRegressor,
+    # Creation of EI Regression with Random Forest
+    eiReg = EmbeddedInterpreter(regressor=RandomForestRegressor,
                                 model_optimizer=regressor_optimizer,
                                 model_preprocessor=None,
                                 n_buckets=n_buckets,
@@ -62,7 +60,7 @@ def execute(save_dir, n_buckets=3, i=None, bucketing_method="quantile", single_r
                                 reg_hp_args=regressor_hp_grid,
                                 max_iter=4000, lossfn="MSE",
                                 min_dloss=0.0001, lr=0.005, precompute_rules=True,
-                                force_precompute=True, device="cpu")
+                                force_precompute=True, device="cuda")
 
     eiReg.fit(X_train, y_train,
               add_single_rules=True, single_rules_breaks=single_rules_breaks, add_multi_rules=True,
@@ -94,8 +92,7 @@ def execute(save_dir, n_buckets=3, i=None, bucketing_method="quantile", single_r
 
 def run_multiple_executions(save_dir, num_buckets, num_iterations, dataset_name, single_rules_breaks=3):
     os.makedirs(save_dir, exist_ok=True)
-    all_results_file_path = os.path.join(save_dir,
-                                         f"{dataset_name}_results_{num_buckets}_buckets_{num_iterations}_iterations.json")
+    all_results_file_path = os.path.join(save_dir, f"results_{num_buckets}_buckets_{num_iterations}_iterations.json")
 
     all_results = {}
     # Check if the consolidated results file exists and load it
@@ -108,11 +105,12 @@ def run_multiple_executions(save_dir, num_buckets, num_iterations, dataset_name,
 
         for iteration in range(1, num_iterations + 1):
             # Construct the expected path for the results of this iteration
-            expected_result_path = os.path.join(save_dir, "rules", f"rule_results_{n_buckets}_buckets_{iteration}_iterations.txt")
+            expected_result_path = os.path.join(save_dir, "rules",
+                                                f"{dataset_name}_results_{num_buckets}_buckets_{num_iterations}_iterations.json")
 
             # Check if this experiment's results already exist
             if not os.path.exists(expected_result_path):
-                print(f"Running execution for {n_buckets} buckets, iteration {iteration}")
+                print(f"Running execution for {n_buckets} bucket(s), iteration {iteration}")
                 results = execute(save_dir=save_dir, n_buckets=n_buckets, i=iteration, single_rules_breaks=single_rules_breaks)
                 bucket_results.append(results)
                 all_results[f"{n_buckets}_buckets"] = bucket_results
@@ -121,7 +119,7 @@ def run_multiple_executions(save_dir, num_buckets, num_iterations, dataset_name,
 
 
 if __name__ == '__main__':
-    save_dir = "examples/XGBRegression/results/delta_elevators_3_breaks"
+    save_dir = "examples/RFRegression/results/delta_elevators_3_breaks"
     run_multiple_executions(save_dir=save_dir,
                             num_buckets=10,
                             num_iterations=50,
